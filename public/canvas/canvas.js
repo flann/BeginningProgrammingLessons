27314cc4da16a57c2f22d18eb29f6464163c47c6
Terry Lacy <terry.lacy@hsc.utah.edu>
2017-11-29 13:54:04 -0700

    Added canvas experiments

--- public/canvas/canvas.js
+++ public/canvas/canvas.js
@@ -9,11 +9,60 @@ function useCanvas() {
 
     var context = canvas.getContext('2d');
 
-    context.fillStyle = 'rgb(200, 0, 0)';
-    context.fillRect(10, 10, 50, 50);
+    context.strokeStyle = 'black';
+    context.lineWidth = 1;
 
-    context.fillStyle = 'rgba(0, 0, 200, 0.75)';
-    context.fillRect(30, 30, 50, 50);
+    context.fillStyle = 'rgb(200, 0, 0)';
+    context.fillRect(10, 10, 150, 150);
+
+    context.fillStyle = 'rgba(0, 0, 200, 0.25)';
+    context.fillRect(50, 50, 150, 150);
+
+    context.strokeRect(220, 100, 200, 100);
+
+    context.beginPath();
+    context.moveTo(575, 150);
+    context.lineTo(650, 175);
+    context.lineTo(650, 105);
+    context.fill();
+
+    context.fillStyle = 'rgba(0, 175, 0, 0.75)';
+    context.beginPath();
+    context.arc(500, 300, 150, 0, Math.PI * 2.0);
+    context.stroke();
+    context.fill();
+
+    context.strokeStyle = 'gray';
+    context.lineWidth = 10;
+    context.lineJoin = 'round';
+    context.setLineDash([5,5]);
+
+    context.beginPath();
+    context.moveTo(850, 275);
+    context.quadraticCurveTo(1100, 100, 1500, 275);
+
+    context.bezierCurveTo(1400, 50, 1000, 50, 850, 275);
+    context.closePath();
+    context.stroke();
+    context.fill();
+
+    var image = new Image();
+    image.src = "/images/photo.jpg";
+    image.onload = function() {
+      // create pattern
+      var pattern = context.createPattern(image, 'repeat');
+      context.fillStyle = pattern;
+      context.fillRect(100, 250, 200, 200);
+    }
+
+    context.font = '50px Courier New';
+    context.fillStyle = 'black';
+    context.fillText("This is some text", 900, 350);
+    context.lineWidth = 1;
+    context.lineJoin = 'miter';
+    context.setLineDash([1,0]);
+    context.strokeStyle = 'rgba(100, 100, 100, 0.50)';
+    context.strokeText("This is some text", 910, 360);
 }
 
 function canvasResize() {
