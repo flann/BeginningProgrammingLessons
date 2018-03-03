8c5197f48cee048198df5edcec15290813ccf475
Kai <kaidebirk@gmail.com>
2018-03-03 10:48:06 -0700

    Added day length to the Sunrise/Sunset page. Probably want to change formatting for day length because it is a little large for the page.

--- public/sunriseSunset/sunriseSunsetRequest.js
+++ public/sunriseSunset/sunriseSunsetRequest.js
@@ -21,13 +21,20 @@ class SunriseSunsetRequest {
             let solarNoonNode = document.getElementById("solarNoon");
             solarNoonNode.innerHTML = "Solar Noon " + this.time2String(data.results.solar_noon);
 
-
+            let dayLengthNode = document.getElementById("dayLength");
+            dayLengthNode.innerHTML = "Day Length " + this.seconds2String(data.results.day_length);
         }
     }
     load() {
         this.request.open("GET", this.url);
         // this.request.responseType = "json";
         this.request.send();
+    }
+    seconds2String(seconds) {
+        let s = seconds % 60;
+        let m = ((seconds - s) / 60) % 60;
+        let h = (seconds - s - (m*60)) /(60*60);
+        return h + " Hours, " + m + " Minutes, " + s + " Seconds";
     }
     time2String(dateString) {
         var date = new Date(dateString);
@@ -37,11 +44,11 @@ class SunriseSunsetRequest {
         }
         var hours = date.getHours();
         let allHours = hours;
-        if (hours>12){
+        if (hours > 12) {
             allHours = allHours - 12;
         }
         var ampm = "PM";
-        if (hours<12){
+        if (hours < 12) {
             ampm = "AM";
         }
         var time = allHours + ":" + minutes + " " + ampm;
